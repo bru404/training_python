ea05d6c690f4f6abddd6b93fdc1442d3738132fc
Nikita Levashov <n.levashov@idaproject.com>
2021-11-04 22:27:05 +0300

    get_details

---
+++ get_details/solution.py
@@ -0,0 +1,6 @@
+def get_details(arr: list) -> str:
+    """
+    Возвращает индекс элемента и его значение
+    в формате Index - index | Element - element
+    """
+    pass
---
+++ get_details/tests_get_details.py
@@ -0,0 +1,12 @@
+from solution import get_details
+
+
+def test_get_details():
+    actual = get_details([1, 2, 3, 4, 5, 6])
+    expected = 'Index - 0 | Element - 1\n'\
+               'Index - 1 | Element - 2\n'\
+               'Index - 2 | Element - 3\n'\
+               'Index - 3 | Element - 4\n'\
+               'Index - 4 | Element - 5\n'\
+               'Index - 5 | Element - 6\n'
+    assert actual == expected
