8c4569bfb0a6937dae0e5a986e1cb69b059c75d0
Nikita Levashov <n.levashov@idaproject.com>
2021-11-05 11:30:48 +0300

    updated tests

--- 5_is_palindrome/tests_is_palindrome.py
+++ 5_is_palindrome/tests_is_palindrome.py
@@ -4,16 +4,16 @@ from solution import is_palindrome
 def test_is_palindrome() -> None:
     actual = is_palindrome('radar')
     expected = True
-    assert actual == expected
+    assert actual == expected, 'Неверное определение палиндрома'
 
     actual = is_palindrome('civic')
     expected = True
-    assert actual == expected
+    assert actual == expected, 'Неверное определение палиндрома'
 
     actual = is_palindrome('Because I am Batman')
     expected = False
-    assert actual == expected
+    assert actual == expected, 'Неверное определение палиндрома'
 
     actual = is_palindrome('')
     expected = True
-    assert actual == expected
+    assert actual == expected, 'Неверное определение палиндрома'
--- 6_get_max_number/tests_get_max_number.py
+++ 6_get_max_number/tests_get_max_number.py
@@ -4,8 +4,8 @@ from solution import get_max_number
 def test_get_max_number() -> None:
     actual = get_max_number([100, 200, 200, 1377])
     expected = 1377
-    assert actual == expected
+    assert actual == expected, 'Неверно найден максимум в списке'
 
     actual = get_max_number([1, 1, 1, 1, 1])
     expected = 1
-    assert actual == expected
+    assert actual == expected, 'Неверно найден максимум в списке'
